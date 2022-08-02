4e8a131a3768714075e000255ed02e768034e8ac
Joe-Olaya <joel-olaya@hotmail.com>
2022-08-01 23:27:42 -0300

    btod

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,11 +2,19 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  var suma = 0;
+  var array = Array.from(num);
+  for (i = array.length; i >= 0; i-- ){
+    if (array[i] === 1){
+      suma += Math.pow(2,i);
+    }
+  }
+  return suma
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  
 
 }
 
