815bd936d752c9bbecd87f963998a4b1a42f9c78
Joe-Olaya <joel-olaya@hotmail.com>
2022-08-02 21:23:39 -0300

    first homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,20 +1,25 @@
 'use strict'
-
 function BinarioADecimal(num) {
   // tu codigo aca
+  var array = num.split('').reverse().join('');
   var suma = 0;
-  var array = Array.from(num);
-  for (i = array.length; i >= 0; i-- ){
-    if (array[i] === 1){
+  for (let i = 0; i < array.length; i++ ){
+    if (array.charAt(i) == 1){
       suma += Math.pow(2,i);
     }
   }
   return suma
+ 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-  
+  var binario = '';
+  while (num != 0){
+    binario += (num%2).toString();
+    num = Math.floor(num/2);
+  }
+  return binario.split('').reverse().join('');
 
 }
 
