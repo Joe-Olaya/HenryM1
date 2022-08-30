1b582b43f1285cbd2fcc0d0c5f5da21f65df6202
Joe-Olaya <joel-olaya@hotmail.com>
2022-08-30 20:30:35 -0300

    HW08

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,15 +6,51 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+let array = [1];
+let cuenta = num;
+let primo = 2;
+  while (cuenta > 1) {
+    if(cuenta%primo===0){
+    array.push(primo);
+    cuenta /= primo;
+    } else {
+    primo ++
+    }
+  }
+  return array
 }
+
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  //let bandera = true;
+  //let guardar = 0;
+  //   while(bandera) {
+  //     bandera = false
+  //     for(let i = 0; i < array.length; i++){
+  //     if(array[i] > array[i+1]){
+  //       guardar = array[i];
+  //       array[i] = array[i+1];
+  //       array[i+1] = guardar;
+  //       bandera = true
+  //     }
+  //   } 
+  // }
+  // return array
+  
+  for (let j = 0; j < array.length; j++){
+    for(let i = 0; i < array.length; i++){
+      if(array[i] > array[i+1]){
+        let guardar = array[i];
+        array[i] = array[i+1];
+        array[i+1] = guardar;
+      }
+    }
+  }
+  return array
 }
 
 
@@ -23,6 +59,17 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for (let i = 1; i < array.length; i++) {
+    let prev = i-1
+    let temp = array[i]
+      while(prev >= 0 && temp < array[prev]){
+        array[prev+1] = array[prev]
+        prev--
+      }
+    array[prev+1] = temp
+  }
+  return array
+
 
 }
 
@@ -32,7 +79,20 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {//asumno que el num mas chico esta en el i
+    let min = i;
+    for (let j = i+1; j < array.length; j++) {//reocrre el array buscando el numero mas chico
+      if(array[min] > array[j]){//actualizamos el index numero mas chico
+        min = j
+      }
+    }
+    if(i !== min){//intercambiamos valores entre el numero mas chico y el "primer" valor
+      let temp = array[i];
+      array[i] = array[min];
+      array[min] = temp
+    }
+  }
+  return array
 }
 
 
--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -1,12 +1,31 @@
 'use strict'
 // No cambies los nombres de las funciones.
 
-function quickSort(array) {
-  // Implementar el método conocido como quickSort para ordenar de menor a mayor
-  // el array recibido como parámetro
-  // Devolver el array ordenado resultante
-  // Tu código:
+// [5, 1, 4, 2, 3,7] 8 [11,24]
+// [1,2,3] 4 [5,7] - 11 [24]
+// [1] 2 [3] - 5 [7]
 
+// [5, 1, 4, 2,  8 ,11,3,24,7]
+function quickSort(array) {
+//   // Implementar el método conocido como quickSort para ordenar de menor a mayor
+//   // el array recibido como parámetro
+//   // Devolver el array ordenado resultante
+//   // Tu código:
+if(array.length <= 1){
+  return array
+}
+let pivot = array[0];
+let izq = [];
+let der = [];
+let auxArray = [];
+  for (let i = 1; i < array.length; i++) {
+    if(array[i] > pivot){
+      der.push(array[i]);
+    } else {
+      izq.push(array[i]);
+    } 
+  }
+  return auxArray.concat(quickSort(izq),pivot,quickSort(der))
 }
 
 function mergeSort(array) {
@@ -15,7 +34,42 @@ function mergeSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  if (array.length <= 1) {
+    return array;
+  }
+
+  let division = middle(array);
+  let left = division[0];
+  let right = division[1];
+  return paste(mergeSort(left), mergeSort(right));
 }
+
+function middle(array) {
+  let mitad = Math.floor(array.length / 2);
+  let izq = array.slice(0, mitad);
+  let der = array.slice(mitad);
+
+  return [izq, der];
+
+}
+
+function paste(left, right) {
+  let array = []
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      array.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      array.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return array.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
